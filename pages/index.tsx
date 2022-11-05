e4b17940ff3db213bddfdf11a762c7cf67b77696
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-05 22:59:13 +0400

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import type {NextPage} from 'next'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen">
       <Head>
         <title>Dmitry's Portfolio</title>
         <link rel='icon' href='/favicon.ico' />
@@ -14,6 +15,9 @@ const Home: NextPage = () => {
       <Header />
 
       {/* Hero */}
+      <section id='hero'>
+        <Hero />
+      </section>
 
       {/* About  */}
 

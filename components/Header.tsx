b325d7edb07401d4f4f41e8c4b5d2254c63178c8
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-05 22:11:29 +0400

    new commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,6 +18,9 @@ export default function Header({}: Props) {
           opacity: 1,
           scale: 1,
         }}
+        transition={{
+          duration: 1.5,
+        }}
         className='flex flex-row items-center'
       >
         <SocialIcon
@@ -37,17 +40,32 @@ export default function Header({}: Props) {
         />
       </motion.div>
 
-      <div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className='flex flex-row items-center text-gray-300 cursor-pointer'
+      >
         <SocialIcon
           className='cursor-pointer'
           network='email'
           fgColor='gray'
           bgColor='transparent'
         />
-        <p className='uppercase hidden md:inline-flex text-sm text-gray-300 cursor-pointer'>
+        <p className='uppercase hidden md:inline-flex text-sm text-gray-400 cursor-pointer'>
           Get In Touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }

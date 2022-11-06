146ff7afd874db56a1be41758609430c98313224
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-07 00:21:19 +0400

    new commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
+import Link from 'next/link';
 
 type Props = {};
 
@@ -18,13 +19,12 @@ export default function Hero({}: Props) {
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
       <BackgroundCircles />
       {/* https://raw.githubusercontent.com/DmitryVelichko/personal-portfolio-website-js/main/images/photo1.png*/}
-
       <img
-        className='relative rounded-full h-32 w-32 mx-auto object-cover'
+        className='relative rounded-full h-32 w-32 mx-auto object-cover '
         src='https://raw.githubusercontent.com/DmitryVelichko/personal-portfolio-website-js/main/images/photo1.jpg'
         alt="A man's face"
       />
-      <div>
+      <div className='z-20'>
         <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
           Software Engineer
         </h2>
@@ -32,13 +32,19 @@ export default function Hero({}: Props) {
           <span className='mr-3'>{text}</span>
           <Cursor cursorColor='#f7ab0a' />
         </h1>
-
-        <div>
-          <button className='heroButton'>About</button>
-          <button className='heroButton'>Experience</button>
-          <button className='heroButton'>Skills</button>
-          <button className='heroButton'>Projects</button>
-       
+        <div className='pt-5'>
+          <Link href='#about'>
+            <button className='heroButton'>About</button>
+          </Link>
+          <Link href='#experience'>
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href='#skills'>
+            <button className='heroButton'>Skills</button>
+          </Link>
+          <Link href='#projects'>
+            <button className='heroButton'>Projects</button>
+          </Link>
         </div>
       </div>
     </div>

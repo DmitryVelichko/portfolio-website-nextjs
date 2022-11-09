60654d46bb9e2d014d9639c5c0088b3f77ef815f
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-09 12:51:17 +0400

    new commit

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
 
-type Props = {}
+type Props = {};
 
 function ExperienceCard({}: Props) {
   return (
     <article>
-      <img src="https://cdn.sanity.io/images/ltuexkre/production/050ee674d199aa8d254af2b5ea480d3dc320cbb1-1240x1440.png" alt="" />
+      <motion.img
+        initial={{
+          y: -100,
+          opacity: 0,
+        }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+        src='https://cdn.sanity.io/images/ltuexkre/production/050ee674d199aa8d254af2b5ea480d3dc320cbb1-1240x1440.png'
+        alt=''
+      />
+
+      <div>
+        <h4>CEO of Company</h4>
+        <p>CompanyName</p>
+        <div>
+          {/*Tech used */}
+          {/*Tech used */}
+          {/*Tech used */}
+        </div>
+        <p>Started work... - Ended...</p>
+        <ul>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+          <li>Summary points</li>
+        </ul>
+      </div>
     </article>
-  )
+  );
 }
 
-export default ExperienceCard
+export default ExperienceCard;

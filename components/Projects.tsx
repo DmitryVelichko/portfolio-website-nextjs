eac736deb588982c32144500d6c209184b0cde8f
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-10 17:20:44 +0400

    new commit

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
 function Projects({}: Props) {
   return (
-    <div>Projects</div>
-  )
+    <div className='h-[120vh] relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+        Projects
+      </h3>
+
+    <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+        {/* projects */}
+        {/* projects */}
+        {/* projects */}
+    </div>
+
+      <div className='w-full absolute top-[30%] bg-[#f7ab0a]/10 left-0 h-[500px] -skew-y-12'/>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;

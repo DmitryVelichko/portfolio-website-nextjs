5689926f6ed413bb5efe06bc91e660e7bc946a51
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-10 17:06:36 +0400

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import Head from 'next/head';
-import type {NextPage} from 'next'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import About from '../components/About'
-import WorkExperience from '../components/WorkExperience'
-import Skills from '../components/Skills'
+import type { NextPage } from 'next';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import WorkExperience from '../components/WorkExperience';
+import Skills from '../components/Skills';
+import Projects from '../components/Skills';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Dmitry's Portfolio</title>
         <link rel='icon' href='/favicon.ico' />
@@ -24,19 +25,20 @@ const Home: NextPage = () => {
         <About />
       </section>
 
-     <section id='experience' className='snap-center'>
-      <WorkExperience />
-     </section>
+      <section id='experience' className='snap-center'>
+        <WorkExperience />
+      </section>
 
-      <section id="skills" className='snap-start'>
+      <section id='skills' className='snap-start'>
         <Skills />
       </section>
 
-      {/* Projects  */}
-
+      <section id='projects' className='snap-start'>
+        <Projects />
+      </section>
       {/* Contact */}
     </div>
   );
-}
+};
 
 export default Home;

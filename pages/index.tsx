0042e6f76d5fddc005d35cc5912faad2571943f9
Dmitry Velichko <soleil-2005@mail.ru>
2022-11-14 23:27:20 +0400

    new commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,8 @@ import ContactMe from '../components/ContactMe'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scrollbar-thin'>
+    // snap-y snap-mandatory
+    <div className='bg-[rgb(36,36,36)] text-white h-screen  overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80 scrollbar-thin'> 
       <Head>
         <title>Dmitry's Portfolio</title>
         <link rel='icon' href='/favicon.ico' />
